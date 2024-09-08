699143a978423a12ab098b4d560fc88ccc831195
zohakhan2018 <zoha_khan@yahoo.com>
2024-09-08 18:25:09 -0400

    9/7 commit

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "my_instance" {
   ami           = "ami-0b0ea68c435eb488d" # Example AMI ID, choose one appropriate for your region and needs
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public[0].id
   associate_public_ip_address = true
 
   tags = {
