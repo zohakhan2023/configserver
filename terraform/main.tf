61e5445b5c74302c5441fb4fb607f34c7eddaade
zohakhan2018 <zoha_khan@yahoo.com>
2024-09-08 15:28:31 -0400

    9/7 commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-      region  = "us-east-1"
+      source = "hashicorp/aws"
     }
+  }
+  #  backend "s3" {
+  #    region = "us-east-1"
+  #    key    = "terraform.tfstate"
+  #  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "Zoha_EC2_instance" {
+  ami           = "ami-0b0ea68c435eb488d"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Zoha_EC2_instance"
   }
 }
-  resource "aws_instance" "EC2_instance" {
-    ami           = "ami-0b0ea68c435eb488d"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "EC2_instance"
-    }
-  }
