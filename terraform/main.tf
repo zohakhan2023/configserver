c3f990f8df01d9f219deeeb282c94bb7f6f29dbf
zohakhan2018 <zoha_khan@yahoo.com>
2024-09-08 15:43:23 -0400

    9/7 commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,10 +15,3 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "Zoha_EC2_instance" {
-  ami           = "ami-0b0ea68c435eb488d"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "Zoha_EC2_instance"
-  }
-}
