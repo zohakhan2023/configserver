58b2cb748854bf5f4c90edf987ebc1a717792376
zohakhan2018 <zoha_khan@yahoo.com>
2024-09-08 18:11:12 -0400

    9/7 commit

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1"
+  availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet"
