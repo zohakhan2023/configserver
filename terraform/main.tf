ec45acb54b7114dedca5555d2ab6bec616a1829e
zohakhan2018 <zoha_khan@yahoo.com>
2024-09-08 15:23:14 -0400

    9/7 commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+      region  = "us-east-1"
     }
   }
-  #  backend "s3" {
-  #    region = "us-east-1"
-  #    key    = "terraform.tfstate"
-  #  }
 }
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
-}
-resource "aws_instance" "EC2_instance" {
-  ami           = "ami-0182f373e66f89c85"
-  instance_type = "t2.nano"
-  tags = {
-    Name = "EC2_instance"
+  resource "aws_instance" "EC2_instance" {
+    ami           = "ami-0b0ea68c435eb488d"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "EC2_instance"
+    }
   }
-}
