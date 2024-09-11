3143a7a63d0826eb943f180050e2424e2aea4e50
zohakhan2018 <zoha_khan@yahoo.com>
2024-09-11 10:54:18 -0400

    9/7 commit

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -8,5 +8,5 @@ resource "aws_s3_bucket" "terraform_state" {
 
 resource "aws_s3_bucket_acl" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
-  acl    = "public"
+  acl    = "private"
 }
