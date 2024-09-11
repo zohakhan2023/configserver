f7e323d6859f6b43e4f0ad8e7f4d3d37c170538a
zohakhan2018 <zoha_khan@yahoo.com>
2024-09-11 13:07:01 -0400

    9/7 commit

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,7 +6,18 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-resource "aws_s3_bucket_acl" "terraform_state" {
+resource "aws_s3_bucket_policy" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
-  acl    = "private"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "s3:GetObject"
+        Effect    = "Allow"
+        Resource  = "${aws_s3_bucket.terraform_state.arn}/*"
+        Principal = "*"
+      }
+    ]
+  })
 }
